aa9ceec9a503e7019524fa564f39580f1e3a61e6
dongpt <dong.pham@arrowhitech.com>
2023-12-29 11:37:07 +0700

    update exercise_8

---
+++ week1/exercise_8.ts
@@ -0,0 +1,7 @@
+import { factorial, isPrime } from './utils';
+
+const num1 = 5;
+const num2 = 13;
+
+console.log(`Factorial of ${num1}: ${factorial(num1)}`);
+console.log(`Is ${num2} prime? ${isPrime(num2) ? 'Yes' : 'No'}`);
